2370127acdb8527c8665a0bb3dd0673c2516970d
KseniyaDmitrieva <dksu82@mail.ru>
2022-12-07 13:59:23 +0300

    add solution for home work task 10,13,15

---
+++ HW2Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Считываем данные с консоли  проверяем на 0 и парсим
+int inputNumber = int.Parse(Console.ReadLine() ?? "0");
+
+// Проверяем является ди введенное число трехзначным 
+if (inputNumber < 1000 && inputNumber > 99)
+{
+    int secondDigit = ((inputNumber / 10) % 10);
+
+    //Определяем воторую цифру и выводим ее
+    Console.WriteLine("Вторая цифра - " + secondDigit);
+}
+else
+{
+    // Выводим сообщение что число не трехзначное
+    Console.WriteLine("Введенное число не трёхзначное");
+}
---
+++ HW2Task13/Program.cs
@@ -0,0 +1,41 @@
+﻿// Считываем данные с консоли  проверяем на 0 и парсим
+//int inputNumber = int.Parse(Console.ReadLine() ?? "0");
+
+// Проверяем есть ли третья цифра 
+//if (inputNumber>99)
+//{
+//Определяем третью цифру и выводим ее
+
+//char[] digits = inputNumber.ToString().ToCharArray();
+//Console.WriteLine("Третья цифра числа  "+inputNumber+" - "+digits[2]);
+//}
+//else
+//{
+// Выводим сообщение что число не трехзначное
+//Console.WriteLine(inputNumber+" - Третьей цифры нет ");
+//}
+
+
+// Вариант 2
+
+// Считываем данные с консоли  проверяем на 0 и парсим
+int inputNumber = int.Parse(Console.ReadLine() ?? "0");
+//Определяем длину введенного числа
+int lengthOfInputNumber = (int)Math.Log(inputNumber, 10) + 1;
+
+//Проверяем есть ли третья цифра в числе
+
+if (lengthOfInputNumber > 2)
+{
+    //Определяем третью цифру и выводим результат
+    int degree = (int)Math.Pow(10, lengthOfInputNumber - 3);
+    int thirdNumber = (inputNumber / degree) % 10;
+    Console.WriteLine("Третья цифра в числе " + inputNumber + " - " + thirdNumber);
+}
+else
+{
+    // Выводим сообщение об отсутствии третьей цифры
+    Console.WriteLine(inputNumber + " - Третьей цифры нет ");
+}
+
+
